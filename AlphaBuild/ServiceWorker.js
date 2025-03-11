9355540fbe2bc1ec3300f9472d301de5f2adb0d1
Yi-Chieh Chiu <56674515+ChiuYeeJay@users.noreply.github.com>
2025-03-11 04:38:59 -0700

    AlphaBuild

---
+++ AlphaBuild/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-2D Golf Prototype-1.0";
+const contentToCache = [
+    "Build/AlphaBuild.loader.js",
+    "Build/AlphaBuild.framework.js.unityweb",
+    "Build/AlphaBuild.data.unityweb",
+    "Build/AlphaBuild.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
