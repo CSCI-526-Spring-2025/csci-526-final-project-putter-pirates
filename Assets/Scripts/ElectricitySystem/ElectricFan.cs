01cc8cd26e52515bafa5a1c75b1ed6383e2b698c
deardisha <dishagoyal2001@gmail.com>
2025-04-26 18:12:14 -0700

    level 7 fixed

--- Assets/Scripts/ElectricitySystem/ElectricFan.cs
+++ Assets/Scripts/ElectricitySystem/ElectricFan.cs
@@ -6,23 +6,39 @@ public class ElectricFan : MonoBehaviour
     bool powered;
     [SerializeField]
     bool isActivatedWhenPowered;
+
     GameObject cross;
     ElectricityComponent electricityComponent;
     AreaEffector2D areaEffector2D;
+    ParticleSystem windParticles; // NEW
 
     void Start()
     {
         cross = transform.Find("Cross").gameObject;
-        areaEffector2D = gameObject.GetComponent<AreaEffector2D>();
-
+        areaEffector2D = GetComponent<AreaEffector2D>();
         electricityComponent = transform.Find("ElectricityTrigger").GetComponent<ElectricityComponent>();
+
+        windParticles = GetComponentInChildren<ParticleSystem>(); // Find the particle system
     }
 
     void Update()
     {
         powered = electricityComponent.isCharged;
         bool fanActivated = isActivatedWhenPowered ? powered : !powered;
+
         cross.SetActive(!fanActivated);
         areaEffector2D.enabled = fanActivated;
+
+        if (windParticles != null)
+        {
+            if (cross.activeSelf && windParticles.isPlaying)
+            {
+                windParticles.Stop();
+            }
+            else if (!cross.activeSelf && !windParticles.isPlaying)
+            {
+                windParticles.Play();
+            }
+        }
     }
 }
