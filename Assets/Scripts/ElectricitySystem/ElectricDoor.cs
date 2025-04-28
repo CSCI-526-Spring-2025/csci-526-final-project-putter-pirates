e4e737e15ef3f10826b219aa885891c63f41b94c
deardisha <dishagoyal2001@gmail.com>
2025-04-27 17:13:21 -0700

    level 7 waterfall completed

--- Assets/Scripts/ElectricitySystem/ElectricDoor.cs
+++ Assets/Scripts/ElectricitySystem/ElectricDoor.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class ElectricDoor : MonoBehaviour
 {
-    Color openedColor = new Color(0.368f, 0, 0.671f, 0.1f);
-    Color closedColor = new Color(0.368f, 0, 0.671f, 1);
+    float fadedAlpha = 0.3f; // Faded but still visible
+    float normalAlpha = 1f;  // Fully visible
     BoxCollider2D boxCollider2D;
     SpriteRenderer spriteRenderer;
+    Animator animator;
     ElectricityComponent electricityComponent;
 
     [SerializeField]
@@ -13,16 +14,29 @@ public class ElectricDoor : MonoBehaviour
 
     void Start()
     {
-        boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         electricityComponent = transform.Find("ElectricityTrigger").GetComponent<ElectricityComponent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        bool powered = electricityComponent.isCharged;
-        boxCollider2D.enabled = !powered;
-        spriteRenderer.color = powered? openedColor : closedColor;
+        powered = electricityComponent.isCharged;
+
+        if (powered)
+        {
+            // Powered: Fade and freeze
+            boxCollider2D.enabled = false; // Allow passage
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, fadedAlpha);
+            if (animator.enabled) animator.enabled = false; // Freeze animation
+        }
+        else
+        {
+            // Not powered: Normal
+            boxCollider2D.enabled = true; // Block passage
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, normalAlpha);
+            if (!animator.enabled) animator.enabled = true; // Resume animation
+        }
     }
 }
