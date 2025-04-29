c8f737e3795c5a484ee7f1a85a383f7cc40c18fd
Yi-Chieh Chiu <56674515+ChiuYeeJay@users.noreply.github.com>
2025-04-29 04:02:48 -0700

    Fix a bug that if player adjust aim assist in title screen, number of dot would not be updated until player click menu in level

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreenManager : MonoBehaviour
 {
     [SerializeField] int defualtNumDot = 10;
+    [SerializeField] private Slider slider;
 
     void Awake()
     {
@@ -12,7 +14,11 @@ public class TitleScreenManager : MonoBehaviour
 
     void Start()
     {
-        
+        slider.onValueChanged.AddListener((v) =>
+        {
+            PlayerPrefs.SetInt("numDots", (int)v);
+            PlayerPrefs.Save(); // Save persistently
+        });
     }
 
     void Update()
